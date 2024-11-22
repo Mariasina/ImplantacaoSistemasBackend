0f0c839a18b73048b5d0f90f19865eb8f7ece7bd
Mariasina <carolbeveer@gmail.com>
2024-11-22 10:25:39 -0300

    changing port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('./startup/db')();
 require('./startup/routes')(app);
 
 
-const port = 8080;
+const port = process.env.PORT;
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
